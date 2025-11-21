608aecbcfd424f9078c7ff91da30d86237b40362
Padraic Moriarty <ms0163229@mtu.ie>
2025-11-21 13:00:36 +0000

    Lab 6

---
+++ Lab6/Lab6ex1.py
@@ -0,0 +1,14 @@
+import math
+
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+
+
+if num1 > num2:
+    print("num1 is the max of",num1," and ",num2)
+elif num1 < num2:
+    print("num2 is the max of",num1," and ",num2)
+else:
+    print("The numbers are equal")
+
+
